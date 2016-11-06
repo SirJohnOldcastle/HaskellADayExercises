3fb296cc9389c73d17c698399a15b84e749c420f
Max Parrish <maxparrish@gmail.com>
2016-11-06 08:31:38 -0600

    add expand
    a little silly

--- exercises/HAD/Y2014/M02/D25/Exercise.hs
+++ exercises/HAD/Y2014/M02/D25/Exercise.hs
@@ -1,7 +1,13 @@
 module HAD.Y2014.M02.D25.Exercise where
 
 import Control.Monad
-import Data.List (group)
+import Data.List
 
 compress :: Eq a => [a] -> [(a, Int)]
 compress = fmap (liftM (,) head `ap` length) . group
+
+(.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(.:) = (.) . (.)
+
+expand :: [(a, Int)] -> [a]
+expand = concat .: fmap $ uncurry $ flip replicate
